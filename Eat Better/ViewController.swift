0a48333fc8161ee93a99663ac96afbf657f423bc
Inna <litvinenko.odindvatri@gmail.com>
2020-05-18 21:25:16 +0300

    Initial Commit

---
+++ Eat Better/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eat Better
+//
+//  Created by Inna Litvinenko on 5/18/20.
+//  Copyright © 2020 Inna Litvinenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
